898b05723203632fdd977e4f05c39244b022524b
UtahJimmy <jimmy@cs.utah.edu>
2018-10-27 20:57:47 -0600

    added marker popups and unique IDs

--- js/leafletExample.js
+++ js/leafletExample.js
@@ -4,9 +4,9 @@
 //            VARIABLES
 //
 /////////////////////////////////////
-var circMarkerOptions = {
-    radius:6,
-    title:"test",
+var markerRadius = 6;
+var circMarker1Options = {
+    radius:markerRadius,
     color:'#000000',
     weight:2,
     fill:true,
@@ -14,6 +14,18 @@ var circMarkerOptions = {
     fillOpacity: 0.9,
     interactive:true
 };
+
+var circMarker2Options = {
+    radius:markerRadius,
+    color:'#000000',
+    weight:2,
+    fill:true,
+    fillColor:'#fea211',
+    fillOpacity: 0.9,
+    interactive:true
+};
+
+
 
 var zoomLevel = 11;
 var slcLocation = [40.710,-111.870];
@@ -28,7 +40,7 @@ var senPos = readTextFile(file);
 var senJSON = csvJSON(senPos);
 
 
-var markerList={}
+var markerList={};
 
 
 /////////////////////////////////////
@@ -36,6 +48,7 @@ var markerList={}
 //            STATEMENTS
 //
 /////////////////////////////////////
+
 // Add tile layer to map -- MapBox Streets tile layer
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -45,35 +58,36 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(myMap);
 
 
-
-var circ = L.circleMarker([40.7, -111.899], circMarkerOptions
-)
-    .on('click',clickEvent)
-    .addTo(myMap);
-
-var circ2 = L.circleMarker([40.7, -111.91], circMarkerOptions
-).addTo(myMap);
-
-circ2.on('click',clickEvent) ;
-
 Object.keys(senJSON).forEach(function(k,i){
 
-    var sensor = senJSON[i]
+    var sensor = senJSON[i];
     var lat = parseFloat(sensor.lat);
     var lng = parseFloat(sensor.long);
     var id  = sensor.ID;
     var type = sensor.type;
 
-    console.log(k, lat,lng,id, type)
+   // console.log("ID: " + id + " length = " +id.length);
 
-    markerList[id] = L.circleMarker ([lat,lng], circMarkerOptions).addTo(myMap);
 
-    markerList[id].on("mouseOver",mouseOverEvent);
+    //console.log(k, lat,lng,id, type)
+
+    if(id.length > 5) {
+        markerList[id] = L.circleMarker ([lat,lng], circMarker1Options).addTo(myMap)
+
+    } else{
+        markerList[id] = L.circleMarker ([lat,lng], circMarker2Options).addTo(myMap);
+    }
+
+    markerList[id].bindPopup("<b>monitor: </b>" + id);
+    markerList[id]._path.id = id;
+    //console.log(markerList[id]);
+    //markerList[id].sourceTarget._leaflet_id = id;
+    markerList[id].on("mouseover",mouseOverEvent);
     markerList[id].on("click", clickEvent);
+    markerList[id].on('mouseout',mouseOutEvent);
     //markerList[sensor.ID] = L.circleMarker ([lat,lng], circMarkerOptions).addTo(myMap);
 
 });
-
 
 
 
@@ -122,10 +136,25 @@ function readTextFile(file) {
 }
 
 function clickEvent(e){
-    console.log(this);
-    this.options['fillOpacity']= 0;
+
+    var markerID = e.sourceTarget._path.id;
+    console.log("you clicked " + markerID);
 }
 
 function mouseOverEvent(e){
-    console.log(this);
+
+    var markerID = e.sourceTarget._path.id;
+    this.openPopup();
+    console.log("you hovered " + markerID);
+
+}
+
+
+
+function mouseOutEvent(e){
+
+    var markerID = e.sourceTarget._path.id;
+    this.closePopup();
+    console.log("you hovered " + markerID);
+
 }
